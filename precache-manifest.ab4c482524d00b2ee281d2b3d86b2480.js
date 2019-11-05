7df8dbfc6c2bd998b9bb6577e98f225325ff76c1
muhsin7majeed <muhsin7ak@gmail.com>
2019-11-05 12:45:37 +0530

    Updates

---
+++ precache-manifest.ab4c482524d00b2ee281d2b3d86b2480.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "046418042309303a63a97d97fdfb3a54",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "a0757c74408e6988d0a7",
+    "url": "/tic-tac-toe/static/css/main.e4d644ef.chunk.css"
+  },
+  {
+    "revision": "7144a41f7e6c783a7e97",
+    "url": "/tic-tac-toe/static/js/2.83a1039e.chunk.js"
+  },
+  {
+    "revision": "a0757c74408e6988d0a7",
+    "url": "/tic-tac-toe/static/js/main.d6330a03.chunk.js"
+  },
+  {
+    "revision": "385629666264212be688",
+    "url": "/tic-tac-toe/static/js/runtime-main.9b00db00.js"
+  }
+]);
